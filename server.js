4170c292fbedba42e363ebc0e7cdec014edeaa67
Harshit Shukla <mrharshit2409@gmail.com>
2024-04-18 13:40:33 +0530

    minor fix

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ const server = http.createServer(app);
 
 const io = new Server(server , {
     cors: {
-        origin: "https://code-among-frontend.vercel.app",
+        origin: "https://codeamong.vercel.app/",
         methods: ["GET" , "POST"],
     },
 });
